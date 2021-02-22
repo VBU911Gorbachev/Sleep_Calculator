5a4d3eec1a6e80b475cdbf03ac74882d61ebae0b
Somewhere <66011798+Somwere@users.noreply.github.com>
2021-02-22 22:46:19 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -37,6 +37,7 @@ namespace Sleep_Calculator
             Controls.Add(minutes);
             //=============================================================
             calc_btn = new Button();
+            calc_btn.Click += Calc_btn_Click; 
             calc_btn.Location = new Point(385, 400);
             calc_btn.Size = new Size(100, 50);
             calc_btn.BackColor = Color.FromArgb(58, 110, 252);
@@ -56,15 +57,15 @@ namespace Sleep_Calculator
             Controls.Add(prog_name);
             //=============================================================
             description = new Label();
-            description.Location = new Point(20, 300);
-            description.Width = 500;
+            description.Location = new Point(40, 300);
+            description.Width = 250;
             description.Font = new Font("Gill Sans Ultra Bold", 14);
             description.Text = "Хочу проснуться в . . .";
             description.ForeColor = Color.White;
             Controls.Add(description);
             //=============================================================
             double_dotes = new Label();
-            double_dotes.Location = new Point(3, 300);
+            double_dotes.Location = new Point(425, 300);
             double_dotes.Size = new Size(50, 50);
             double_dotes.Font = new Font("Gill Sans Ultra Bold", 10);
             double_dotes.Text = ":";
@@ -72,18 +73,66 @@ namespace Sleep_Calculator
             Controls.Add(double_dotes);
             //=============================================================
             authors = new Label();
-            authors.Location = new Point(200, 100);
-            authors.Text = "Горбачёв Кирилл & Куваев Никита";
+            authors.Location = new Point(310, 600);
+            authors.Width = 300;
+            authors.Font = new Font("Gill Sans Ultra Bold", 10);
+            authors.Text = "(c) Горбачёв Кирилл, Куваев Никита";
             authors.ForeColor = Color.White;
             Controls.Add(authors);
             //=============================================================
+            result_1 = new Label();
+            result_1.BorderStyle = BorderStyle.Fixed3D;
+            result_1.Location = new Point(205, 525);
+            result_1.Size = new Size(100, 20);
+            result_1.BackColor = Color.FromArgb(23, 43, 99);
+            Controls.Add(result_1);
+            //=============================================================
+            result_2 = new Label();
+            result_2.BorderStyle = BorderStyle.Fixed3D;
+            result_2.Location = new Point(325, 525);
+            result_2.Size = new Size(100, 20);
+            result_2.BackColor = Color.FromArgb(23, 43, 99);
+            Controls.Add(result_2);
+            //=============================================================
+            result_3 = new Label();
+            result_3.BorderStyle = BorderStyle.Fixed3D;
+            result_3.Location = new Point(445, 525);
+            result_3.Size = new Size(100, 20);
+            result_3.BackColor = Color.FromArgb(23, 43, 99);
+            Controls.Add(result_3);
+            //=============================================================
+            result_4 = new Label();
+            result_4.BorderStyle = BorderStyle.Fixed3D;
+            result_4.Location = new Point(565, 525);
+            result_4.Size = new Size(100, 20);
+            result_4.BackColor = Color.FromArgb(23, 43, 99);
+            Controls.Add(result_4);
+            //=============================================================
+            res_name = new Label();
+            res_name.Location = new Point(365, 495);
+            res_name.Width = 150;
+            res_name.Text = "Вам лучше лечь в:";
+            res_name.Font = new Font("Gill Sans Ultra Bold", 10);
+            Controls.Add(res_name);
+            //=============================================================
         }
+
+        private void Calc_btn_Click(object sender, EventArgs e)
+        {
+            Sleep_Hours(e);
+        }
+
         ComboBox hours;
         ComboBox minutes;
+        Label result_1;
+        Label result_2;
+        Label result_3;
+        Label result_4;
         Button calc_btn;
         Label prog_name;
         Label description;
         Label double_dotes;
         Label authors;
+        Label res_name;
     }
 }
