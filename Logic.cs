c8be4d55d924f4ec8b41cb96c8f22290731fb440
Somewhere <66011798+Somwere@users.noreply.github.com>
2021-02-23 12:38:11 +0200

    Update Logic.cs

--- Logic.cs
+++ Logic.cs
@@ -1,32 +1,93 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace Sleep_Calculator
 {
     public partial class Form1 : Form
     {
-        public int sum1 = 0;
-        public int sum2 = 0;
-        public int sum3 = 0;
-        public int sum4 = 0;
+        public int sum_hour_1 = 0, sum_min_1 = 0;
+        public int sum_hour_2 = 0, sum_min_2 = 0;
+        public int sum_hour_3 = 0, sum_min_3 = 0;
+        public int sum_hour_4 = 0, sum_min_4 = 0;
         public int res1 = 0;
         public int res2 = 0;
         public int res3 = 0;
         public int res4 = 0;
         public void Sleep_Hours(EventArgs e)
         {
-            sum1 = Convert.ToInt32(hours.Text);
-            sum2 = Convert.ToInt32(hours.Text);
-            sum3 = Convert.ToInt32(hours.Text);
-            sum4 = Convert.ToInt32(hours.Text);
-            res1 = sum1 - 6;
-            res2 = sum2 - 7;
-            res3 = sum3 - 8;
-            res4 = sum4 - 9;
-            result_1.Text = res1.ToString();
-            result_2.Text = res2.ToString();
-            result_3.Text = res3.ToString();
-            result_4.Text = res4.ToString();
+            sum_hour_1 = Convert.ToInt32(hours.Text);
+            sum_min_1 = Convert.ToInt32(minutes.Text);
+            sum_hour_2 = Convert.ToInt32(hours.Text);
+            sum_min_2 = Convert.ToInt32(minutes.Text);
+            sum_hour_3 = Convert.ToInt32(hours.Text);
+            sum_min_3 = Convert.ToInt32(minutes.Text);
+            sum_hour_4 = Convert.ToInt32(hours.Text);
+            sum_min_4 = Convert.ToInt32(minutes.Text);
+            res1 = sum_hour_1 - 6;
+            res2 = sum_hour_2 - 7;
+            res3 = sum_hour_3 - 9;
+            res4 = sum_hour_4 - 10;
+            if (sum_min_1 == 0 && sum_hour_1 == 0 && sum_hour_2 == 0 && sum_min_2 == 0 && sum_hour_3 == 0 && sum_min_3 == 0 && sum_hour_4 == 0 && sum_min_4 == 0)
+            {
+                res1 = 24 - 6;
+                res2 = 24 - 7;
+                res3 = 24 - 9;
+                res4 = 24 - 10;
+            }
+            if (sum_hour_1 < 6 && sum_hour_2 < 6 && sum_hour_3 < 6 && sum_hour_4 < 6)
+            {
+                res1 = (24 + sum_hour_1) - 6;
+                res2 = (24 + sum_hour_2) - 7;
+                res3 = (24 + sum_hour_3) - 9;
+                res4 = (24 + sum_hour_4) - 10;
+            }
+            if (res1 < 0 || res2 < 0 || res3 < 0 || res4 < 0)
+            {
+                res1 = 24 - (6 - sum_hour_1);
+                res2 = 24 - (7 - sum_hour_2);
+                res3 = 24 - (9 - sum_hour_3);
+                res4 = 24 - (10 - sum_hour_4);
+            }
+            if (sum_hour_1 == 6)
+            {
+                res1 = 0;
+            }
+            if (sum_hour_1 == 7)
+            {
+                res1 = 1;
+                res2 = 0;
+            }
+            if (sum_hour_1 == 8)
+            {
+                res1 = 2;
+                res2 = 1;
+            }
+            if (sum_hour_1 == 9)
+            {
+                res1 = 3;
+                res2 = 2;
+                res3 = 0;
+            }
+            result_1.Text = res1.ToString() + ":" + sum_min_1.ToString();
+            if (sum_min_1 < 10 || sum_min_1 == 0)
+            {
+                result_1.Text = res1.ToString() + ": 0" + sum_min_1.ToString();
+            }
+            result_2.Text = res2.ToString() + ":" + sum_min_2.ToString();
+            if (sum_min_2 < 10 || sum_min_2 == 0)
+            {
+                result_2.Text = res2.ToString() + ": 0" + sum_min_2.ToString();
+            }
+            result_3.Text = res3.ToString() + ":" + sum_min_3.ToString();
+            if (sum_min_3 < 10 || sum_min_3 == 0)
+            {
+                result_3.Text = res3.ToString() + ": 0" + sum_min_3.ToString();
+            }
+            result_4.Text = res4.ToString() + ":" + sum_min_4.ToString();
+            if (sum_min_4 < 10 || sum_min_4 == 0)
+            {
+                result_4.Text = res4.ToString() + ": 0" + sum_min_4.ToString();
+            }
         }
     }
 }
